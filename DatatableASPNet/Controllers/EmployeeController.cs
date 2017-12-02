5885a6acc21dbef788fcfb55bea1ca336c6314b2
donniegrubat2005 <donniegrubat2005@gmail.com>
2017-12-02 20:15:41 +0800

    first commit

---
+++ DatatableASPNet/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DatatableASPNet.Models;
+
+namespace DatatableASPNet.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmployeeContext dbemployee = new EmployeeContext();
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetEmployee()
+        {
+            var empList = dbemployee.Employees.ToList<Employee>();
+            return Json(new { data=empList},JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ DatatableASPNet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DatatableASPNet.MvcApplication" Language="C#" %>
